e0b5843a4e3d5ec51faa138ac1704fd2dd8a1c53
Oliver Dry <olliedry1999@hotmail.co.uk>
2020-06-18 10:16:29 +0100

    First Commit

---
+++ BlackOps4.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZombiesRound
+{
+    /// <summary>
+    /// Interaction logic for BlackOps4.xaml
+    /// </summary>
+    public partial class BlackOps4 : Window
+    {
+        public BlackOps4()
+        {
+            InitializeComponent();
+        }
+
+        private void Alpha_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 1;
+        }
+
+        private void Ancient_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 2;
+        }
+
+        private void IX_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 3;
+        }
+
+        private void voy_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 4;
+        }
+
+        private void Blood_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 5;
+        }
+
+        private void Call_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 8;
+        }
+
+        private void Class_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 6;
+        }
+
+        private void Dead_btn_Click(object sender, RoutedEventArgs e)
+        {
+            tc_BO4.SelectedIndex = 7;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZombiesRound
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void BO2_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Bo4 = new BlackOps4();
+            Bo4.tc_BO4.SelectedIndex = 0;
+            this.Close();
+            Bo4.Show();
+        }
+
+        private void BO3btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show");
+        }
+
+        private void BO1btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show");
+        }
+    }
+}
